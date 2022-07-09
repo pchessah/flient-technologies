35a8f463585bfdd3575c09c5bab36ea2bde5ddb6
Paul Chesa <paulchesa1@gmail.com>
2022-07-09 15:17:39 +0300

    🎨Finish content addition to site

--- projects/features/src/lib/pages/landing-page/landing-page.component.ts
+++ projects/features/src/lib/pages/landing-page/landing-page.component.ts
@@ -20,4 +20,8 @@ export class LandingPageComponent implements OnInit {
   getInternetTechImg = () => 'assets/landing-page/internet-tech.jpg';
 
   getWebDesignImg = () => 'assets/landing-page/web-design.svg';
+
+  getAvatar = () => 'assets/landing-page/avatar-1.svg';
+
+  getLogo = () => 'https://cdn.icon-icons.com/icons2/1738/PNG/512/iconfinder-technologymachineelectronicdevice10-4026450_113340.png';
 }
